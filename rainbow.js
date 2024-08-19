d6dcd61ff073d7c784c4acc24fe34ef9cca7324d
lzzwscjmx <101620079+lzzwscjmx@users.noreply.github.com>
2024-08-20 02:32:44 +0800

    Initial Commit

---
+++ rainbow.js
@@ -0,0 +1,64 @@
+function calculateRainbowNumber() {
+    let birthdate = document.getElementById('birthdate').value;
+    if (!birthdate) {
+        document.getElementById('result').textContent = "请输入有效的出生日期";
+        return;
+    }
+    // 将日期分解为年、月、日
+    let [year, month, day] = birthdate.split('-').map(Number);
+
+    // 使用Lunar.js将阳历转换为阴历
+    let lunar = Solar.fromYmd(year, month, day).getLunar();
+
+    let lunarYear = lunar.getYear();
+    let lunarMonth = lunar.getMonth();
+    let lunarDay = lunar.getDay();
+
+       // 阳历彩虹数字计算
+       let solarSum = calculateSum(birthdate);
+       let solarRainbowNumber = formatRainbowNumber(solarSum);
+   
+       // 阴历彩虹数字计算
+       let lunarSum = calculateSum(`${lunarYear}${lunarMonth}${lunarDay}`);
+       let lunarRainbowNumber = formatRainbowNumber(lunarSum);
+   
+       document.getElementById('result').innerHTML = 
+           `阳历彩虹数字: +${solarRainbowNumber} <br> 阴历彩虹数字: -${lunarRainbowNumber}`;
+   }
+   
+function calculateSum(dateString) {
+    let digits = dateString.replace(/-/g, '').split('');
+    return digits.reduce((acc, digit) => acc + parseInt(digit), 0);
+}
+
+function formatRainbowNumber(num) {
+    // 计算第一次的和
+    let firstSum = num;
+    
+    // 如果和是两位数，继续简化
+    let intermediateSum = reduceToTwoDigits(firstSum);
+    
+    // 计算最终的单一位数字
+    let finalSum = reduceToSingleDigit(intermediateSum);
+    
+    // 根据情况决定显示的格式
+    if (firstSum >= 10 && intermediateSum >= 10) {
+        return `${firstSum}/${intermediateSum}/${finalSum}`;
+    } else {
+        return `${firstSum}/${finalSum}`;
+    }
+}
+
+function reduceToTwoDigits(num) {
+    if (num >= 10) {
+        return num.toString().split('').reduce((acc, digit) => acc + parseInt(digit), 0);
+    }
+    return num; 
+}
+
+function reduceToSingleDigit(num) {
+    while (num >= 10) {
+        num = num.toString().split('').reduce((acc, digit) => acc + parseInt(digit), 0);
+    }
+    return num;
+}
